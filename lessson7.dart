cc1df37c8d6be6aa4193c4eb15dd294b1459aa55
aigeram <118278771+aigeram@users.noreply.github.com>
2022-12-01 23:42:04 +0600

    lesson7.dart

---
+++ lessson7.dart
@@ -0,0 +1,74 @@
+
+
+void main(){
+  int i=1;
+  while(i<=100){
+    print(i);
+    ++i;
+    }
+
+    //trite1
+    int t=1;
+    for(int t=1;t<=100;t++){
+      print(t);
+    }
+
+
+
+
+
+
+
+
+// //trite3
+ int a=11;
+     while(a<=33){
+       print(a);
+       ++a;
+     }
+
+
+
+// //trite4
+   int v=11;
+     for(int v=11;v<=33;v++){
+       print(v);
+     }
+
+//     //trite5
+
+     int b=0;
+     for(int b=0; b<100;b++){
+       if(b.isEven){
+         print(b);
+       }
+     }
+
+//     //trite6
+    int c=0;
+     while( c<=100){
+       if( c .isEven){
+         print(c++);
+        
+       }
+       }
+
+
+//trite7
+
+
+ int f=1;
+ int sum= 0;
+
+ for(int f=1;f<=100;f++){
+   sum = sum +f;
+   print("$sum + $f = ${sum + f}");
+   print(f);
+ }
+ print("sum == $sum");
+}
+
+
+
+
+  
